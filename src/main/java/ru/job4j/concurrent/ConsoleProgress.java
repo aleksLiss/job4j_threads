af62caa0eeea203014d84af5cd611b0dd56753e7
aleksei <darklisik.darklisik@yandex.ru>
2024-12-20 16:39:06 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,33 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        var process = new char[]{'-', '\\', '|', '/'};
+        int index = 0;
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.print("\rLoading ... " + process[index]);
+                index++;
+                if (index == 4) {
+                    index = 0;
+                }
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(5000);
+            progress.interrupt();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
