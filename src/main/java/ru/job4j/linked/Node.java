2e41b2d10aea0ed3101694f770c859fc8992358b
aleksei <darklisik.darklisik@yandex.ru>
2024-12-25 16:35:12 +0300

    3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,18 @@
+package ru.job4j.linked;
+public final class Node<T> {
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() throws CloneNotSupportedException {
+        return (Node<T>) next.clone();
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
